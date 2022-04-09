edf2b37d708d3beb3c85847d6d8aa7f216f81ebb
AleksandrVasilko <avasilko7@gmail.com>
2022-04-09 23:04:40 +0300

    Edit from class to hooks

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,58 +1,65 @@
-import React from "react";
+import { useState } from "react";
 import Controls from './Controls';
 import StaticRes from './StaticRes'
 import s from './Controls.module.css'
 
 
 
-class Statistics extends React.Component {
+export default function Statistics () {
+    
+    const [good, setGood] = useState(0);
+    const[neutral, setNeutral] = useState(0); 
+    const [bad, setBad] = useState(0); 
+    
 
-    state = {
-        good: 0,
-        neutral: 0,
-        bad: 0
-    }
-
-    handleClick = control => {
-		this.setState({ [control]: this.state[control] + 1 });
+    const handleClick = control => {
+        switch (control) {
+            case 'good':
+                setGood(prevState => prevState + 1);
+                break;
+            case 'neutral':
+                setNeutral(prevState => prevState + 1);
+                break;
+            case 'bad':
+                setBad(prevState => prevState + 1);
+                break;
+            default:
+                return;
+        }
 	};
     
     
-    countTotalFeedback = () => {
-        const { good, bad, neutral } = this.state;
+    const countTotalFeedback = () => {
         return (good + bad + neutral)
     }
 
-    countPositiveFeedbackPercentage = () => {
-        const { good, bad, neutral } = this.state;
+    const countPositiveFeedbackPercentage = () => {
         let result = (good / (good + bad + neutral) * 100);
         return (isNaN(result) ? 0 : result)
     }
-
-    render() {
-        const { good, neutral, bad } = this.state
-        const total = this.countTotalFeedback();
-        const rating = Math.round(this.countPositiveFeedbackPercentage());
+        
+    const total = countTotalFeedback();
+    const rating = Math.round(countPositiveFeedbackPercentage());
        
         
-        return (
-            <div className="Statistics">
-                <p className={s.title}>Please leave feetback</p>
-                <Controls
-                    options={['good', 'neutral', 'bad']}
-                    onHandleClick={this.handleClick}
-                />
-                <p className={s.title}>Statistics</p>
-                <StaticRes
-                    good={good}
-                    bad={bad}
-                    neutral={neutral}
-                    totalFeedback={total}
-                    goodPercent={rating}
-                />  
-            </div>
-        )
-    }
+    return (
+        <div className="Statistics">
+            <p className={s.title}>Please leave feetback</p>
+            <Controls
+                options={['good', 'neutral', 'bad']}
+                onHandleClick={handleClick}
+            />
+            <p className={s.title}>Statistics</p>
+            <StaticRes
+                good={good}
+                bad={bad}
+                neutral={neutral}
+                totalFeedback={total}
+                goodPercent={rating}
+            />  
+        </div>
+    )
 }
 
-export default Statistics;
+
+
